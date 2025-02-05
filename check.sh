84b2af7f706f3db1c80b353c48eb8f23227fe9dd
clayton <ashleyclayton@student.unsw.edu.au>
2025-02-05 17:14:46 +1100

    added nice indenting to the checker

--- check.sh
+++ check.sh
@@ -4,15 +4,40 @@
 # Usage example: bash ../check.sh
 
 # Description: Tests all solutions in solutions/accepted against
-# expected output. Green for AC (Accepted), red for WA (Wrong Answer), yellow for TLE (Time Limit Exceeded). 
+# expected output. Green for AC (Accepted), red for WA 
+# (Wrong Answer), yellow for TLE (Time Limit Exceeded). 
 
 time_limit=1
 
 i=1
 col_sep=0
 in_names=()
+in_files=()
 sol_names=()
 ex=()
+
+repeat() {
+    for i in $(seq $1); do
+	echo -n "$2"
+    done
+}
+
+check() {
+    x=$1
+    sol_name=$2
+    file=$3
+    expected=$4
+
+    actual=$(timeout $time_limit $x < $file)
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        printf "\033[1;93m$sol_name\033[0m " # 1;33 for original darker yellow
+    elif diff -wq $expected <(echo $actual) > /dev/null; then
+        printf "\033[1;32m$sol_name\033[0m " 
+    else
+        printf "\033[1;31m$sol_name\033[0m "
+    fi
+}
 
 for sol in solutions/accepted/*; do
     run_solution=./solution
@@ -36,45 +61,36 @@ for sol in solutions/accepted/*; do
     ((i++))
 done
 
-check() {
-    x=$1
-    sol_name=$2
-    file=$3
-    expected=$4
-
-    actual=$(timeout $time_limit $x < $file)
-    exit_code=$?
-    if [ $exit_code -ne 0 ]; then
-        printf "\033[1;93m$sol_name\033[0m " # 1;33 for original darker yellow
-    elif diff -wq $expected <(echo $actual) > /dev/null; then
-        printf "\033[1;32m$sol_name\033[0m " 
-    else
-        printf "\033[1;31m$sol_name\033[0m "
-    fi
-}
-
 for file in $(find data -name "*.in" | sort); do
     in_name=${file#"data/"}
     in_name=${in_name%.in}
-    in_names+=$in_name
+    in_names+=($in_name)
+    in_files+=($file)
     sep=$(echo $in_name | wc -c)
     if [ $sep -gt $col_sep ]; then
         col_sep=$sep
     fi
 done
 
-for file in $(find data -name "*.in" | sort); do
-    expected=${file%in}out
-    in_name=${file#"data/"}
-    in_name=${in_name%.in}
+for i in ${!in_names[@]}; do
+    in_name=${in_names[$i]}
     sep=$(echo $in_name | wc -c)
-    
-    echo -n $in_name ""
+    replacer=$in_name"$(repeat $(($col_sep - $sep)) ' ' )"
+    in_names[$i]=$replacer
+done
 
-    for i in ${!sol_names[@]}; do
-        check "${ex[$i]}" "${sol_names[$i]}" $file $expected
+for i in ${!in_files[@]}; do    
+    in_name=${in_names[$i]}
+    file=${in_files[$i]}
+    expected=${file%in}out
+
+    echo -n "$in_name" ""
+
+    for j in ${!sol_names[@]}; do
+        check ${ex[$j]} ${sol_names[$j]} $file $expected
     done
     echo ""
+
 done
 
 for i in "${ex[@]}"; do
