a3612e117e12830cd3a0e72b5301b135ecc0a883
clayton <ashleyclayton@student.unsw.edu.au>
2025-02-17 17:52:02 +1100

    added ianuary checker from 24-t1 dragon contest

---
+++ ianuary/checkout.py
@@ -0,0 +1,53 @@
+
+def nc2(i):
+    return i * (i - 1) / 2
+
+def get_icount(a):
+    twos = 0
+    total = 0
+    for i in range(len(a)):
+        if a[i] == '2':
+            twos += 1
+        elif a[i] == '4':
+            total += nc2(twos)
+        else:
+            return -1
+    return total
+
+def checker(student_output_file, input_path, output_path):
+    try:
+        student_output = student_output_file.read().split()
+    except:
+        return 0.0
+
+    with open(output_path) as f:
+        judge_output = f.read().split()
+
+    with open(input_path) as f:
+        inputt = [i.strip().split() for i in f.readlines()[1:]]
+        for i in range(len(inputt)):
+            inputt[i] = [int(j) for j in inputt[i]]
+
+    i = 0
+    j = 0
+    k = 0
+    while (i < len(student_output) and j < len(judge_output) and k < len(inputt)):
+        if (student_output[i] != judge_output[j]):
+            return 0.0
+        elif (student_output[i] == 'NO'):
+            i += 1
+            j += 1
+            k += 1
+        elif (student_output[i] == 'YES'):
+            i += 1
+            j += 1
+            if (len(student_output[i]) != inputt[k][0]):
+                return 0.0
+            if (get_icount(student_output[i].strip()) != inputt[k][1]):
+                return 0.0
+            i += 1
+            j += 1
+            k += 1
+        else:
+            return 0.0
+    return 1.0
