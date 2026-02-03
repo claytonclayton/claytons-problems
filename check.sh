80e4d1cf7aafc61bd29f444787ff14273f0d8893
Ashley Clayton <ashman2600@gmail.com>
2026-02-04 10:42:16 +1100

    checker now uses full basename (does not trim file extension). implemented haskell checker

--- check.sh
+++ check.sh
@@ -43,9 +43,9 @@ print_res() {
     if [ $res = AC ]; then
         printf "\033[1;32m$sol_name\033[0m " 
     elif [ $res = TLE ]; then
-	    printf "\033[1;93m$sol_name\033[0m " # 1;33 for original darker yellow
+	      printf "\033[1;93m$sol_name\033[0m " # 1;33 for original darker yellow
     else
-	    printf "\033[1;31m$sol_name\033[0m " 
+	      printf "\033[1;31m$sol_name\033[0m " 
     fi
 }
 
@@ -92,20 +92,22 @@ fi
 for sol in $(find "$sol_dir" -type f); do
     run_solution=./solution
     if [[ $sol == *.cpp ]]; then
-        # g++ -O2 -std=c++20 $sol -o $i
-        g++-15 -std=gnu++20 $sol -o $i
+        g++ -O2 -std=c++20 $sol -o $i
         ex+=(./$i)
     elif [[ $sol == *.c ]]; then
         gcc -O2 -lm $sol -o $i
         ex+=(./$i)
     elif [[ $sol == *.py ]]; then
         ex+=("python3 $sol")
+    elif [[ $sol == *.hs ]]; then
+        ghc -O2 -no-keep-hi-files -no-keep-o-files $sol -o $i 
+        ex+=(./$i)
     else
         continue
     fi
 
     short=$(basename $sol)
-    short=${short%.*}
+    #short=${short%.*}
     sol_names+=($short)
     ((i++))
 done
